4c1d86b283039c7405141e4b01ff13ccd530e527
pedGonca <pedro.goncalves@madeiramadeira.com.br>
2022-06-27 14:37:52 -0300

    Arquivos atualizados e formatados

--- codigos_r/códigos_dani.r
+++ codigos_r/códigos_dani.r
@@ -7,6 +7,7 @@
 library(tidyverse)
 library(survival)
 
+
 # Lendo os dados e analisando sua estrutura
 path <- 'base_de_dados/hantavir2.n280.2.08.05.17.csv'
 dados <- read.csv(path, 
@@ -16,74 +17,82 @@ str(dados)
 
 
 
-tempo <- as.numeric(as.Date(dat$DATAOBITO,"%d/%m/%Y")-as.Date(dat$DATA1SINT,"%d/%m/%Y"))
-aux=dat$CASOCONTROLE==2
+tempo <- as.numeric(as.Date(dados$DATAOBITO,"%d/%m/%Y")-as.Date(dados$DATA1SINT,"%d/%m/%Y"))
+aux=dados$CASOCONTROLE==2
 
-tempo[aux] <- as.numeric(as.Date(dat$DATAALTA[aux],"%d/%m/%Y")-as.Date(dat$DATA1SINT[aux],"%d/%m/%Y"))
+tempo[aux] <- as.numeric(as.Date(dados$DATAALTA[aux],"%d/%m/%Y")-as.Date(dados$DATA1SINT[aux],"%d/%m/%Y"))
 
-dat$tempo=tempo
+dados$tempo <- tempo
 
-aux <- is.na(dat$tempo)
+aux <- is.na(dados$tempo)
 
-dat$tempo[aux] <- as.numeric(as.Date(dat$DATAENCERR[aux],"%d/%m/%Y")-as.Date(dat$DATA1SINT[aux],"%d/%m/%Y"))
-aux <- is.na(dat$tempo)
-dat$tempo[aux] <- 60
-aux <- is.na(dat$tempo)
+dados$tempo[aux] <- as.numeric(as.Date(dados$DATAENCERR[aux],"%d/%m/%Y")-as.Date(dados$DATA1SINT[aux],"%d/%m/%Y"))
+aux <- is.na(dados$tempo)
 
-aux <- dat$tempo<0
-dat[aux,]
+dados$tempo[aux] <- 60
+aux <- is.na(dados$tempo)
 
-dat$tempo[aux] <- as.numeric(as.Date(dat$DATAENCERR[aux],"%d/%m/%Y")-as.Date(dat$DATA1SINT[aux],"%d/%m/%Y"))
-aux <- dat$tempo==0
+aux <- dados$tempo < 0
+dados[aux,]
+
+dados$tempo[aux] <- as.numeric(as.Date(dados$DATAENCERR[aux],"%d/%m/%Y")-as.Date(dados$DATA1SINT[aux],"%d/%m/%Y"))
+aux <- dados$tempo == 0
 sum(aux)
-dat$tempo[aux]=1
-dat$censura=ifelse(dat$CASOCONTROLE==1,1,0)
+dados$tempo[aux] <- 1
+dados$censura = ifelse(dados$CASOCONTROLE == 1,1,0)
 
-summary(dat$tempo)
-hist(dat$tempo)
+summary(dados$tempo)
 
-aux=dat$tempo>100
-dat$tempo[aux]=as.numeric(as.Date(dat$DATAENCERR[aux],"%d/%m/%Y")-as.Date(dat$DATA1SINT[aux],"%d/%m/%Y"))
+par(mfrow = c(1,1))
+hist(dados$tempo)
 
-aux=dat$tempo>100
-dat$tempo[aux]=80
+aux=dados$tempo>100
+dados$tempo[aux] <- as.numeric(as.Date(dados$DATAENCERR[aux],"%d/%m/%Y")-as.Date(dados$DATA1SINT[aux],"%d/%m/%Y"))
+
+aux <- dados$tempo>100
+dados$tempo[aux]<- 80
 
 require(survival)
-plot(survfit(Surv(tempo,censura)~1,data=dat))
+plot(survfit(Surv(tempo,censura) ~ 1,
+            data=dados))
 
 #vermelho:nao azul:sim
-plot(survfit(Surv(tempo,censura)~SANGRESREG,data=dat),col=c(2,4))
-plot(survfit(Surv(tempo,censura)~HIPOTENSAOREG,data=dat),col=c(2,4))
-plot(survfit(Surv(tempo,censura)~TONTURAREG,data=dat),col=c(2,4))
+plot(survfit(Surv(tempo,censura)~SANGRESREG,data=dados),col=c(2,4))
+plot(survfit(Surv(tempo,censura)~HIPOTENSAOREG,data=dados),col=c(2,4))
+plot(survfit(Surv(tempo,censura)~TONTURAREG,data=dados),col=c(2,4))
 
-fit=coxph(Surv(tempo,censura)~IDADE+SEXOREG+TONTURAREG+INSUFRENALREG+SANGRESREG+CEFALEIAREG+HIPOTENSAOREG+MIALGIASREG+SINAISHEMOREG+HEMMAIOR46REG+LEUCCDEREG+AUMENTOUREREG+DERPLEURALREG+INFPULDIFREG+EDEMAPULMREG,data=dat)
+fit <- coxph(Surv(tempo,censura)~IDADE+SEXOREG+TONTURAREG+INSUFRENALREG+SANGRESREG+CEFALEIAREG+HIPOTENSAOREG+MIALGIASREG+SINAISHEMOREG+HEMMAIOR46REG+LEUCCDEREG+AUMENTOUREREG+DERPLEURALREG+INFPULDIFREG+EDEMAPULMREG,data=dados)
 summary(fit)
-fit=coxph(Surv(tempo,censura)~IDADE+SEXOREG+TONTURAREG+INSUFRENALREG+SANGRESREG+CEFALEIAREG+HIPOTENSAOREG+MIALGIASREG+SINAISHEMOREG+HEMMAIOR46REG+LEUCCDEREG+AUMENTOUREREG+DERPLEURALREG+INFPULDIFREG,data=dat)
+fit <- coxph(Surv(tempo,censura)~IDADE+SEXOREG+TONTURAREG+INSUFRENALREG+SANGRESREG+CEFALEIAREG+HIPOTENSAOREG+MIALGIASREG+SINAISHEMOREG+HEMMAIOR46REG+LEUCCDEREG+AUMENTOUREREG+DERPLEURALREG+INFPULDIFREG,data=dados)
 summary(fit)
-fit=coxph(Surv(tempo,censura)~IDADE+SEXOREG+TONTURAREG+INSUFRENALREG+SANGRESREG+CEFALEIAREG+HIPOTENSAOREG+MIALGIASREG+SINAISHEMOREG+HEMMAIOR46REG+AUMENTOUREREG+DERPLEURALREG+INFPULDIFREG,data=dat)
+fit <- coxph(Surv(tempo,censura)~IDADE+SEXOREG+TONTURAREG+INSUFRENALREG+SANGRESREG+CEFALEIAREG+HIPOTENSAOREG+MIALGIASREG+SINAISHEMOREG+HEMMAIOR46REG+AUMENTOUREREG+DERPLEURALREG+INFPULDIFREG,data=dados)
 summary(fit)
-fit=coxph(Surv(tempo,censura)~IDADE+SEXOREG+TONTURAREG+SANGRESREG+CEFALEIAREG+HIPOTENSAOREG+MIALGIASREG+SINAISHEMOREG+HEMMAIOR46REG+AUMENTOUREREG+DERPLEURALREG+INFPULDIFREG,data=dat)
+fit <- coxph(Surv(tempo,censura)~IDADE+SEXOREG+TONTURAREG+SANGRESREG+CEFALEIAREG+HIPOTENSAOREG+MIALGIASREG+SINAISHEMOREG+HEMMAIOR46REG+AUMENTOUREREG+DERPLEURALREG+INFPULDIFREG,data=dados)
 summary(fit)
-fit=coxph(Surv(tempo,censura)~IDADE+SEXOREG+TONTURAREG+SANGRESREG+HIPOTENSAOREG+MIALGIASREG+SINAISHEMOREG+HEMMAIOR46REG+AUMENTOUREREG+DERPLEURALREG+INFPULDIFREG,data=dat)
+fit <- coxph(Surv(tempo,censura)~IDADE+SEXOREG+TONTURAREG+SANGRESREG+HIPOTENSAOREG+MIALGIASREG+SINAISHEMOREG+HEMMAIOR46REG+AUMENTOUREREG+DERPLEURALREG+INFPULDIFREG,data=dados)
 summary(fit)
-fit=coxph(Surv(tempo,censura)~IDADE+SEXOREG+TONTURAREG+SANGRESREG+HIPOTENSAOREG+MIALGIASREG+SINAISHEMOREG+HEMMAIOR46REG+AUMENTOUREREG+INFPULDIFREG,data=dat)
+fit <- coxph(Surv(tempo,censura)~IDADE+SEXOREG+TONTURAREG+SANGRESREG+HIPOTENSAOREG+MIALGIASREG+SINAISHEMOREG+HEMMAIOR46REG+AUMENTOUREREG+INFPULDIFREG,data=dados)
 summary(fit)
-fit=coxph(Surv(tempo,censura)~IDADE+SEXOREG+TONTURAREG+SANGRESREG+HIPOTENSAOREG+SINAISHEMOREG+HEMMAIOR46REG+AUMENTOUREREG+INFPULDIFREG,data=dat)
+fit <- coxph(Surv(tempo,censura)~IDADE+SEXOREG+TONTURAREG+SANGRESREG+HIPOTENSAOREG+SINAISHEMOREG+HEMMAIOR46REG+AUMENTOUREREG+INFPULDIFREG,data=dados)
 summary(fit)
-fit=coxph(Surv(tempo,censura)~IDADE+SEXOREG+TONTURAREG+SANGRESREG+HIPOTENSAOREG+SINAISHEMOREG+HEMMAIOR46REG+AUMENTOUREREG,data=dat)
+fit <- coxph(Surv(tempo,censura)~IDADE+SEXOREG+TONTURAREG+SANGRESREG+HIPOTENSAOREG+SINAISHEMOREG+HEMMAIOR46REG+AUMENTOUREREG,data=dados)
 summary(fit)
-fit=coxph(Surv(tempo,censura)~IDADE+SEXOREG+TONTURAREG+SANGRESREG+HIPOTENSAOREG+HEMMAIOR46REG+AUMENTOUREREG,data=dat)
+fit <- coxph(Surv(tempo,censura)~IDADE+SEXOREG+TONTURAREG+SANGRESREG+HIPOTENSAOREG+HEMMAIOR46REG+AUMENTOUREREG,data=dados)
 summary(fit)
 
 #AUMENTOUREREG tem 109 dados omissos
-fit=coxph(Surv(tempo,censura)~IDADE+SEXOREG+TONTURAREG+SANGRESREG+HIPOTENSAOREG+HEMMAIOR46REG,data=dat)
+fit <- coxph(Surv(tempo,censura)~IDADE+SEXOREG+TONTURAREG+SANGRESREG+HIPOTENSAOREG+HEMMAIOR46REG,
+            data=dados)
 summary(fit)
-fit=coxph(Surv(tempo,censura)~IDADE+SEXOREG+TONTURAREG+SANGRESREG+HIPOTENSAOREG,data=dat)
+fit <- coxph(Surv(tempo,censura)~IDADE+SEXOREG+TONTURAREG+SANGRESREG+HIPOTENSAOREG,
+            data=dados)
 summary(fit)
 
 
 require(smcure)
-pd <- smcure(Surv(tempo,censura)~IDADE+SEXOREG+TONTURAREG+SANGRESREG+HIPOTENSAOREG,cureform=~IDADE+SEXOREG+TONTURAREG+SANGRESREG+HIPOTENSAOREG,data=dat,model='ph',nboot=500)
+pd <- smcure(Surv(tempo,censura)~IDADE+SEXOREG+TONTURAREG+SANGRESREG+HIPOTENSAOREG,cureform=~IDADE+SEXOREG+TONTURAREG+SANGRESREG+HIPOTENSAOREG,
+            data=dados,model='ph',nboot=500)
 
-pd <- smcure(Surv(tempo,censura)~IDADE+SEXOREG+TONTURAREG+SANGRESREG+HIPOTENSAOREG,cureform=~IDADE+TONTURAREG+SANGRESREG+HIPOTENSAOREG,data=dat,model='ph',nboot=500)
+pd <- smcure(Surv(tempo,censura)~IDADE+SEXOREG+TONTURAREG+SANGRESREG+HIPOTENSAOREG,cureform=~IDADE+TONTURAREG+SANGRESREG+HIPOTENSAOREG,
+            data=dados,model='ph',nboot=500)
 
